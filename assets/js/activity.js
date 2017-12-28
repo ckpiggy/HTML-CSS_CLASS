48b1266e37631cb792b40ba2f26c3c93f35bddc7
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-12-28 11:09:10 +0800

    add menu

--- assets/js/activity.js
+++ assets/js/activity.js
@@ -23,3 +23,22 @@ function actCreateHtmlDemoZone (defaultText) {
   output.className = 'output'
   return zone
 }
+
+function createHoverMenu (items) {
+  var anchors = items.map(function (item) {
+    var a = document.createElement('a');
+    a.innerText = item.label;
+    a.href = item.href
+    return a
+  });
+  var menu = document.createElement('div');
+  var title = document.createElement('span')
+  title.textContent = 'MENU';
+  title.className = 'title';
+  menu.className = 'menu';
+  menu.appendChild(title)
+  anchors.forEach(function (a) {
+    menu.appendChild(a);
+  });
+  return menu;
+}
